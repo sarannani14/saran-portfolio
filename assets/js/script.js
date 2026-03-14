85cb89485bdc129807472c83e9373661bde0de8c
sarannani14 <sarannani14@gmail.com>
2026-03-14 11:40:51 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
 'use strict';
 
-// 1. Element toggle function
+// Element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-// 2. Sidebar variables (Mobile)
+// Sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// Sidebar toggle functionality for mobile
 if (sidebarBtn) {
   sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 }
 
-// 3. Page navigation variables (The Tabs)
+// Page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
@@ -20,8 +19,12 @@ const pages = document.querySelectorAll("[data-page]");
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
+    // Get the text of the button, remove any accidental spaces, and make it lowercase
+    let clickedTab = this.innerText.toLowerCase().trim();
+
     for (let j = 0; j < pages.length; j++) {
-      if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
+      // Check if the button matches the data-page attribute
+      if (clickedTab === pages[j].dataset.page.trim()) {
         pages[j].classList.add("active");
         navigationLinks[j].classList.add("active");
         window.scrollTo(0, 0);
@@ -30,23 +33,5 @@ for (let i = 0; i < navigationLinks.length; i++) {
         navigationLinks[j].classList.remove("active");
       }
     }
-
   });
-}
-
-// 4. Contact Form validation
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
-
-if (form && formBtn) {
-  for (let i = 0; i < formInputs.length; i++) {
-    formInputs[i].addEventListener("input", function () {
-      if (form.checkValidity()) {
-        formBtn.removeAttribute("disabled");
-      } else {
-        formBtn.setAttribute("disabled", "");
-      }
-    });
-  }
 }
